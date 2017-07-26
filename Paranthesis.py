a7ad2ab0bb084f3ebc290ef629171132e636e287
Atul Bharti <abharti@google.com>
2017-07-26 16:14:06 +0530

    New DS

--- Paranthesis.py
+++ Paranthesis.py
@@ -1,4 +1,47 @@
+from Stack import Stack
+import re
 def is_balanced(p_str):
     if p_str is None or p_str == '':
         return True
-    stack = Sta
+    p_dict = {'(':')'}
+    stack = Stack()
+    newStr = p_str.replace(' ','')
+    newStr = re.sub(r'[a-z]*','',newStr)
+
+    for s in newStr :
+
+        if not stack.is_empty() and s == p_dict[stack.peek()]:
+            stack.pop()
+        else:
+            stack.push(s)
+
+    if stack.is_empty():
+        return True
+    else:
+        return False
+
+
+
+def is_balanced_generic(p_str):
+    if p_str is None or p_str == '':
+        return True
+    p_dict = {'(':')','[':']','{':'}',')':'',']':'','}':''}
+    stack = Stack()
+    newStr = p_str.replace(' ','')
+    newStr = re.sub(r'[a-z]*','',newStr)
+
+    for s in newStr :
+
+        if not stack.is_empty() and s == p_dict[stack.peek()]:
+            stack.pop()
+        else:
+            stack.push(s)
+
+    if stack.is_empty():
+        return True
+    else:
+        return False
+
+
+print(is_balanced_generic('{[)(]}{}'))
+
