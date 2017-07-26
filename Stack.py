6db1e498e6bdba6a85714fb2061f6fb033293c45
Atul Bharti <abharti@google.com>
2017-07-26 16:14:29 +0530

    New DS

--- Stack.py
+++ Stack.py
@@ -4,10 +4,10 @@ class Stack:
     def is_empty(self):
         return self.items == []
     def push(self, item):
-        self.items.insert(0, item)
+        self.items.append(item)
     def pop(self):
-        return self.items.pop(0)
+        return self.items.pop()
     def peek(self):
-        return self.items[0]
+        return self.items[len(self.items)-1]
     def size(self):
         return len(self.items)
